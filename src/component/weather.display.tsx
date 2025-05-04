b284c3774de20ea1a3faf58e24aa56d449627e7b
BlackpearlJack <lestercarlyle@gmail.com>
2025-05-04 12:17:10 +0300

    `Refactor WeatherDisplay component: add styles and content`

--- src/component/weather.display.tsx
+++ src/component/weather.display.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
-
 const WeatherDisplay = () => {
   return (
-    <div className="">
-        Weather Display
+    <div className="p-10">
+      <p className="text-sm uppercase">the.weather</p>
+      <h1 className="text-7xl font-bold mt-10">16°</h1>
+      <p className="text-3xl font-light">London</p>
+      <p className="text-sm mt-2">06:09 - Monday, 9 Sep '19</p>
+      <p className="flex items-center mt-4">
+        <span className="mr-2">☁️</span> Cloudy
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default WeatherDisplay
+export default WeatherDisplay;
